d89b03017dca45929508fecdc867684c736c79ab
Kaleb Koelle <kkoelle@student.fullsail.edu>
2024-05-12 00:14:44 -0400

    Moving around files

---
+++ app/routes/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const router = express.Router();
+
+// localhost:3000/api/
+router.get("/",(req,res)=>{
+    res.status(200).json({
+        message: "GET to API",
+        metadata: {
+            hostname: req.hostname, 
+            method: req.method,
+        }
+    })
+});
+
+router.get("/:id", (req,res)=>{
+    const { id } = req.params;
+    res.
+    status(200)
+    .json({
+        message: "GET by Id for /api",
+        metadata: { hostname: req.hostname, method: req.method},
+    });
+});
+
+router.post("/", (req,res)=>{
+   const { data } = req.body;
+   res.status(200).json({message: "POST to /api", 
+   data,
+   metadata: {hostname: req.hostname, method: req.method},
+})
+})
+
+module.exports = router;
