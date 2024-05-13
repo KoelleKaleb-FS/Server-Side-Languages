694c70399781c4cb580a55a3917bfe029fda2e41
Kaleb Koelle <kkoelle@student.fullsail.edu>
2024-05-13 15:06:29 -0400

    New test for the string module -all passed

---
+++ string/string.js
@@ -0,0 +1,9 @@
+const uppercase = (str) => str.toUpperCase();
+
+// lowercase funtion to lowercase text
+const lowercase = (str) => str.toLowerCase();
+
+module.exports = {
+    uppercase,
+    lowercase
+}
